0758a43050ed4a27ca472187db42311a0065e713
Tomasz Mizera <arezimt@gmail.com>
2024-11-16 15:43:36 +0100

    umlang-4: variables

--- umlang-1.rkt
+++ umlang-1.rkt
@@ -6,15 +6,19 @@
 ;; An Exp denotes an expression in the "arithmetic" language in the
 ;; Evaluation Arithmetic chapter of PLAI. It is one of:
 
+;; - a (ref Symbol), stands for a variable reference
+;; - a (let1 Symbol Exp Exp), makes 'name' available in 'body'
 ;; - a (num Number), stands for a literal number
 ;; - a (bool Boolean), stands for a literal boolean
 ;; - a (plus Exp Exp), denotes addition of the results of two expressions
 ;; - a (conditional Exp Exp Exp), an if/then/else expression
 
+(struct ref (name) #:transparent)
+(struct let1 (name init body) #:transparent)
 (struct num (n) #:transparent)
 (struct bool (b) #:transparent)
 (struct plus (l r) #:transparent)
-(struct conditional (test if-true if-false))
+(struct conditional (test if-true if-false) #:transparent)
 
 
 ;; EXAMPLES
@@ -45,14 +49,41 @@
 (struct v-bool (b) #:transparent)
 
 
-;; calc : Exp -> Value
+;; An Env denotes an *Environment*, mapping names (Symbols) to Values.
+;; It is a ListOF<pair Symbol Value>
+(struct pair (name Value) #:transparent) 
+
+;; empty-env : Env
+(define empty-env '())
+
+;; extend-env: Symbol Value Env -> Env
+(define (extended-env name value env)
+  (cons (pair name value) env)
+)
+
+;; lookup: Env Symbol -> Value
+(define (lookup env name)
+  (match env
+    ['() (error 'lookup "Unbound variable: ~a" name)]
+    [(cons (pair n v) rest) 
+      (if (equal? name n)
+        v
+        (lookup rest name)
+      )
+    ]
+  )
+)
+
+;; calc : Exp Env -> Value
 ;; Computes the result of the given 'exp'.
-(define (calc exp)
+(define (calc exp env)
   (match exp
+    [(ref name) (lookup env name)]
+    [(let1 name init body) (calc body (extended-env name (calc init env) env))]
     [(num n) (v-num n)]
     [(bool b) (v-bool b)]
-    [(plus left right) (primitive-add (calc left) (calc right))]
-    [(conditional test if-true if-false) (if (truthy? (calc test)) (calc if-true) (calc if-false))] 
+    [(plus left right) (primitive-add (calc left env) (calc right env))]
+    [(conditional test if-true if-false) (if (truthy? (calc test env)) (calc if-true env) (calc if-false env))] 
   )
 )
 
@@ -84,12 +115,8 @@
 )
 
 
-(module+ test
-  (check-equal? (calc (num 0)) (v-num 0))
-  (check-equal? (calc (num -5.6)) (v-num -5.6))
-  (check-equal? (calc (plus (num 3) (num 4))) (v-num 7))
-  (check-equal? (calc (plus (num 1) (plus (num 2) (num 3)))) (v-num 6))
-)
+;; ~read : String -> S-Expression
+;; ~parse : S-Expression -> AST
 
 ;; parse-exp : S-Expression -> Exp
 ;; Parse the concrete S-Expression to an instance of Exp, if possible.
@@ -98,15 +125,30 @@
   (match s
     [(list '+ L R) (plus (parse-exp L) (parse-exp R))]
     [(list 'if Test If-True If-False) (conditional (parse-exp Test) (parse-exp If-True) (parse-exp If-False))]
+    [(list 'let1 (list Name Init) Body) (let1 Name (parse-exp Init) (parse-exp Body))]
     [(? boolean? B) (bool B)]                                   
     [(? number? N) (num N)]
+    [(? symbol? S) (ref S)]
     [_ (error 'parse-exp "Parse error: ~v" s)]
   )
 )
 
 
+(module+ test
+  (check-equal? (parse-exp `x) (ref 'x))
+  (check-equal? (parse-exp 0) (num 0))
+  (check-equal? (parse-exp #t) (bool #t))
+  (check-equal? (parse-exp -5.6) (num -5.6))
+  (check-equal? (parse-exp `{+ 3 4}) (plus (num 3) (num 4)))
+  (check-equal? (parse-exp `{+ 1 {+ 2 3}}) (plus (num 1) (plus (num 2) (num 3))))
+  (check-equal? (parse-exp `{if #t 1 2}) (conditional (bool #t) (num 1) (num 2)))
+  (check-equal? (parse-exp `{let1 {x 123} x}) (let1 'x (num 123) (ref 'x))))
+
+
 ;; Parser S-expression syntax examples:
 
+;; x -> (ref 'x)
+;; {let1 {x 123} x} -> (let1 'x (num 123) (ref 'x))
 ;; `{+ 1 2} -> (plus 1 2)
 ;; `{+ 1 {+ 2 3}} -> (plus 1 (plus 2 3))
 ;; `{if #t 1 2} -> (conditional (bool #t) (num 1) (num 2))
@@ -114,20 +156,11 @@
 ;; 1 -> (num 1)
 
 
-(module+ test
-  (check-equal? (parse-exp 0) (num 0))
-  (check-equal? (parse-exp -5.6) (num -5.6))
-  (check-equal? (parse-exp `{+ 3 4}) (plus (num 3) (num 4)))
-  (check-equal? (parse-exp `{+ 1 {+ 2 3}}) (plus (num 1) (plus (num 2) (num 3))))
-  (check-exn #px"Parse error" (lambda () (parse-exp `{1 + 2})))
-)
-
-
 ;; run: S-Expression -> Value
 ;; Parses and then calculates.
 ;; Partil: Parsing may fail; interpretation may fail.
 (define (run s)
-  (calc (parse-exp s))
+  (calc (parse-exp s) empty-env)
 )
 
 (module+ test
@@ -138,7 +171,8 @@
   (check-equal? (run `{+ 1 {+ 2 3}}) (v-num 6))
   (check-equal? (run `{if #t 1 2}) (v-num 1))
   (check-equal? (run `{if #f 1 2}) (v-num 2))
+  (check-equal? (run `{let1 {x 123} x}) (v-num 123))
+
   ;; Let's try some negative tests: valid S-expressions, not parseable into `Exp`s.
   (check-exn #px"Parse error" (lambda () (run `{1 + 2})))
-  (check-exn #px"Parse error" (lambda () (run `{if #t then 1 else 2})))
-)
+  (check-exn #px"Parse error" (lambda () (run `{if #t then 1 else 2}))))
